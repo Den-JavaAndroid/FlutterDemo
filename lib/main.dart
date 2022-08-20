b8e258c0881d17818f3c8675cffe58e2d1c1fe8c
Denis <jaddev888@gmail.com>
2022-08-20 12:26:01 +0300

    Using Provider for State Management in Flutter

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'provider/movie_provider.dart';
+
+import 'screens/home_screen.dart';
+//example from https://www.kindacode.com/article/using-provider-for-state-management-in-flutter/#single__comments
+void main() {
+  runApp(ChangeNotifierProvider<MovieProvider>(
+    child: const MyApp(),
+    create: (_) => MovieProvider(), // Create a new ChangeNotifier object
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Kindacode.com',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
