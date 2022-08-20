bdb9f9631d0a454835f68d423ca2145b77b6172b
Denis <jaddev888@gmail.com>
2022-08-20 15:44:57 +0300

    add animated route

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -29,8 +29,24 @@ class _HomeScreenState extends State<HomeScreen> {
             ElevatedButton.icon(
               onPressed: () {
                 Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const MyListScreen(),
+                  PageRouteBuilder(
+                    pageBuilder: (context,animation, secondaryAnimation)=>const MyListScreen(),
+                    transitionsBuilder: (context, animation, secondaryAnimation, child){
+                      const begin = Offset(0.0, 1.0);
+                      const end = Offset.zero;
+                      const curve = Curves.ease;
+
+                      final tween = Tween(begin: begin, end: end);
+                      final curvedAnimation = CurvedAnimation(
+                        parent: animation,
+                        curve: curve,
+                      );
+
+                      return SlideTransition(
+                        position: tween.animate(curvedAnimation),
+                        child: child,
+                      );
+                    }
                   ),
                 );
               },
